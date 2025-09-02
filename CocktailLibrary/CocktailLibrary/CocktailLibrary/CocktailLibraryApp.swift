9821cc353b429196323322f63cb773252bd67b1e
Sola <pooloom069@gmail.com>
2025-09-02 12:43:03 -0700

    fix nested repo issue

---
+++ CocktailLibrary/CocktailLibrary/CocktailLibrary/CocktailLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailLibraryApp.swift
+//  CocktailLibrary
+//
+//  Created by Sola Lhim on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CocktailLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
