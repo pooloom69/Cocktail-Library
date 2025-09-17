774a443008181c630d513161f20e78591d45e7e7
Sola Lhim <131292558+pooloom69@users.noreply.github.com>
2025-09-17 08:37:41 -0700

    Add files via upload

---
+++ Cocktail-Library/Cocktail-Library/Cocktail_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cocktail_LibraryApp.swift
+//  Cocktail-Library
+//
+//  Created by Sola Lhim on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Cocktail_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
